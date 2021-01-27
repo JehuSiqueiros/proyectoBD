b7de207d5cf6316a6f52ebf1599fceedc7f49cd3
Bárbara Alcaraz <barbara_ag1999@outlook.com>
2021-01-27 16:23:34 -0700

    ya por favor dios :(

--- ProyectoKBI/clsBitacora.cs
+++ ProyectoKBI/clsBitacora.cs
@@ -10,6 +10,7 @@ namespace ProyectoKBI
 {
     class clsBitacora : Conexion
     {
+
         public struct dato
         {
             public int idBitacora;
@@ -76,15 +77,39 @@ namespace ProyectoKBI
         }
 
 
+
+        decimal porcentajeMujeres, porcentajeHombres, porcentajeNiños, porcentajeNiñas;
+        public decimal PorcentajeMujeres
+        {
+            get { return porcentajeMujeres; }
+            set { porcentajeMujeres = value; }
+        }
+        public decimal PorcentajeHombres
+        {
+            get { return porcentajeHombres; }
+            set { porcentajeHombres = value; }
+        }
+        public decimal PorcentajeNiños
+        {
+            get { return porcentajeNiños; }
+            set { porcentajeNiños = value; }
+        }
+        public decimal PorcentajeNiñas
+        {
+            get { return porcentajeNiñas; }
+            set { porcentajeNiñas = value; }
+        }
+
         public void Registrar(string fecha, string hora, int CantHombres, int CantMujeres, int CantNinos, int CantNinas, int RegistradoPor)
         {
             string registro = $"INSERT INTO Bitacora(Fecha, Hora,CantHombres,CantMujeres,CantNinos,CantNinas,RegistradoPor) VALUES('{fecha}','{hora}',{CantHombres},{CantMujeres},{CantNinos},{CantNinas},{RegistradoPor});";
 
-            comando.CommandText = registro;
             AbrirConexion();
+            comando.CommandText = registro;
             comando.ExecuteNonQuery();
             CerrarConexion();
         }
+
 
 
         ArrayList lista = new ArrayList();
@@ -117,7 +142,7 @@ namespace ProyectoKBI
         public ArrayList ConsultarBitacoraMes(int mes)
         {
             AbrirConexion();
-            comando.CommandText = $"SELECT Fecha, Hora, CantHombres, CantMujeres, CantNinos, CantNinas,Nombres as Voluntario FROM Bitacora INNER JOIN Voluntarios ON Bitacora.RegistradoPor = Voluntarios.idVoluntario WHERE MONTH(Fecha) = " + mes ;
+            comando.CommandText = $"SELECT Fecha, Hora, CantHombres, CantMujeres, CantNinos, CantNinas,Nombres as Voluntario FROM Bitacora INNER JOIN Voluntarios ON Bitacora.RegistradoPor = Voluntarios.idVoluntario WHERE MONTH(Fecha) = " + mes;
             SqlDataReader objLectura = comando.ExecuteReader();
 
             while (objLectura.Read())
@@ -138,6 +163,26 @@ namespace ProyectoKBI
             conexion.Close();
             return lista;
         }
+
+        public void ConsultarEstadisticas(string fechaInicio, string fechaFin)
+        {
+            string query = $"SELECT dbo.fnHombresPrc('{fechaInicio}', '{fechaFin}') as hombres," +
+                                  $"dbo.fnMujeresPrc('{fechaInicio}', '{fechaFin}') as mujeres," +
+                                  $"dbo.fnNinasPrc(  '{fechaInicio}', '{fechaFin}') as niñas," +
+                                  $"dbo.fnNinosPrc(  '{fechaInicio}', '{fechaFin}') as niños";
+
+            AbrirConexion();
+            comando.CommandText = query;
+            SqlDataReader lector = comando.ExecuteReader();
+            lector.Read();
+            this.PorcentajeHombres = Convert.ToDecimal(lector["hombres"]);
+            this.PorcentajeMujeres = Convert.ToDecimal(lector["mujeres"]);
+            this.PorcentajeNiñas = Convert.ToDecimal(lector["niñas"]);
+            this.PorcentajeNiños = Convert.ToDecimal(lector["niños"]);
+            CerrarConexion();
+
+        }
+
 
     }
 }
